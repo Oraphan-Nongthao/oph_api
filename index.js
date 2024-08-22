17253548aff49107c51326a35336e566cdc4e7aa
Oraphan Nongthao <Oraphan.nongt@gmail.com>
2024-08-22 14:41:36 +0700

    Update report_qa

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 const mysql = require('mysql2')
-const connection = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'deep_sea'
-})
-//console.log(process.env.USER)
-//up to server
-/*const connection = mysql.createConnection ({
-    host: 'mariadb',
-    user: 'oph',
-    password: 'buopen@dm1n2024',
-    database: 'oph'
-})*/
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database successfully!');
-});
 const express = require('express')
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
@@ -47,10 +26,31 @@ app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 const port = process.env.PORT|5000
 
-/*var data_exposter = require('json2csv')
-const { resolve } = require('path')
-const { rejects } = require('assert')*/
+const connection = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'deep_sea'
+})
+//console.log(process.env.USER)
+//up to server
+/*const connection = mysql.createConnection ({
+    host: 'mariadb',
+    user: 'oph',
+    password: 'buopen@dm1n2024',
+    database: 'oph'
+})*/
 
+function check() {
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connecting to the database:', err);
+            return;
+        }
+        console.log('Connected to the database successfully!');
+    });
+}
+check();
 
 //-------------------------------------Status-------------------------------------//
 //Endpoint to get all status 
@@ -947,7 +947,7 @@ app.get('/report_register' , (req, res) => {
 
             //Export data to excel
             // Set headers to prompt a file download
-            res.setHeader('Content-Type', 'text/csv charset=UTF-8');
+            res.setHeader('Content-Type', 'text/csv');
             res.setHeader('Content-Disposition', 'attachment; filename="RegisterReport_' + date + month + year + '_' + Date.now() + '.csv"');
 
             // Create a writable stream that pipes directly to the response
@@ -980,7 +980,7 @@ app.get('/report_qa' , (req, res) => {
     let year = date_time.getFullYear();
 
     connection.query(
-        '',
+        'SELECT user_id, email_name,q_student,q_parent,answer,score,time FROM qa_transaction LEFT JOIN register_user ON qa_transaction.user_id = register_user.register_id LEFT JOIN qa_question ON qa_transaction.qa_id = qa_question.qa_id LEFT JOIN qa_answers ON qa_transaction.ans_id = qa_answers.ans_id',
         function(err, results){
             if(err){
                 return res.status(500).json({error: err.message});
@@ -1005,7 +1005,7 @@ app.get('/report_qa' , (req, res) => {
 
             //Export data to excel
             // Set headers to prompt a file download
-            res.setHeader('Content-Type', 'text/csv charset=UTF-8');
+            res.setHeader('Content-Type', 'text/csv ');
             res.setHeader('Content-Disposition', 'attachment; filename="QAReport_' + date + month + year + '_' + Date.now() + '.csv"');
 
             // Create a writable stream that pipes directly to the response
