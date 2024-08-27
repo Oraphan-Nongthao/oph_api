8eafd0894e7b4522bdbdb2ed5223f65a5e389247
Oraphan Nongthao <Oraphan.nongt@gmail.com>
2024-08-27 15:15:15 +0700

    test

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const connection = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'deep_sea'
+    database: 'oph'
 })
 //console.log(process.env.USER)
 //up to server
