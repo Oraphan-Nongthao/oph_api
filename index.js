ebb5c9ee9141dfd34c7b8b072ce781cc54b8424d
Oraphan Nongthao <Oraphan.nongt@gmail.com>
2024-08-28 16:17:17 +0700

    test

--- index.js
+++ index.js
@@ -1078,7 +1078,7 @@ app.get('/results/:id', async (req, res) => {
 app.get('/result_max/:id', async (req, res) => {
     try {
         await checkConnection(); // ตรวจสอบการเชื่อมต่อก่อน
-        const [results] = await sequelize.query('SELECT * FROM register_status');
+        const [results] = await sequelize.query('SELECT SUM(score),program_id FROM `qa_transaction` LEFT JOIN qa_answers ON qa_transaction.ans_id = qa_answers.ans_id WHERE user_id=1 GROUP BY program_id ORDER BY SUM(score) DESC LIMIT 1');
         res.json(results);
     } catch (err) {
         res.status(500).json({ error: err.message });
